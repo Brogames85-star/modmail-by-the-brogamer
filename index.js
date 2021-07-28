34f6b7f91d894022a4c079c26c35cac176f82cc4
Brogames85-star <83004175+Brogames85-star@users.noreply.github.com>
2021-07-28 10:34:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+ res.send("hello hell!")
+})
+
+app.listen(3000, () => {
+ console.log("Whatever you want to put here")
+})
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { token,thumb,modmailServerId,ticketCategoryID,modmailLogChannelId,mainServerId } = require('./config.json');
